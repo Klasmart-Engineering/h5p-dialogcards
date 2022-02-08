8172e982d113032900a070b1d55e604fb8e00988
Oliver Tacke <oliver@snordian.de>
2022-02-08 08:34:53 +0000

    Merged in H5P-391 (pull request #4)
    H5P-391
    
    * H5P-391 Apply KLL height limiting
    
    * H5P-391 Fix height limiting
    
    * H5P-391 Fix orientation detection
    
    * H5P-391 Fix height limiting on iPad
    
    * feat(h5p-391): keep regular font size
    
    
    Approved-by: Colt Bauman

--- src/scripts/h5p-dialogcards.js
+++ src/scripts/h5p-dialogcards.js
@@ -142,6 +142,14 @@ class Dialogcards extends H5P.EventDispatcher {
       });
     }
 
+    /*
+     * Workaround (hopefully temporary) for KidsLoopLive that for whatever
+     * reason does not use h5p-resizer.js.
+     */
+    window.addEventListener('resize', () => {
+      this.resize();
+    });
+
     /**
      * Attach h5p inside the given container.
      *
@@ -757,18 +765,35 @@ class Dialogcards extends H5P.EventDispatcher {
 
       let displayLimits = null;
 
+      const titleHeight = this.$inner.find('.h5p-dialogcards-title').outerHeight() || 0;
+      const descriptionHeight = this.$inner.find('.h5p-dialogcards-description').outerHeight(true) || 0;
+      const footerHeight = this.$footer.outerHeight() || 0;
+
+      const nonCardHeight = titleHeight + descriptionHeight + footerHeight;
+
+      const isMobileLandscape = this.isMobileLandscape();
+
       // Change landscape layout on mobile
       if (
         typeof window.orientation === 'number' &&
         this.$cardwrapperSet.find('.h5p-dialogcards-summary-screen').hasClass('h5p-dialogcards-gone') &&
         this.params.behaviour.scaleTextNotCard
       ) {
-        this.$inner.toggleClass('h5p-landscape', window.orientation === 90);
+        this.$inner.toggleClass('h5p-landscape', isMobileLandscape);
 
-        if (window.orientation === 90) {
-          displayLimits = this.computeDisplayLimits();
+        if (isMobileLandscape) {
+          displayLimits = (this.isRoot()) ? this.computeDisplayLimitsKLL() : null;
+
+          const screenHeight = (this.isIOS() && isMobileLandscape) ?
+            screen.width :
+            screen.height;
+
+          if (displayLimits?.height === 1) {
+            displayLimits.height = screenHeight;
+          }
+
           if (displayLimits?.height) {
-            this.$cardwrapperSet.css('max-height', `${displayLimits.height / 2}px`);
+            this.$cardwrapperSet.css('max-height', `${displayLimits.height - nonCardHeight}px`);
           }
           else {
             this.$cardwrapperSet.css('max-height', '');
@@ -781,7 +806,7 @@ class Dialogcards extends H5P.EventDispatcher {
 
       let maxHeight = 0;
 
-      if (!displayLimits || window.orientation !== 90 || !this.params.behaviour.scaleTextNotCard) {
+      if (!displayLimits || !isMobileLandscape || !this.params.behaviour.scaleTextNotCard) {
         this.updateImageSize();
       }
       else {
@@ -794,7 +819,7 @@ class Dialogcards extends H5P.EventDispatcher {
         this.determineCardSizes();
       }
 
-      if (!this.params.behaviour.scaleTextNotCard || window.orientation !== 90 || !displayLimits?.height || !this.$cardwrapperSet.find('.h5p-dialogcards-summary-screen').hasClass('h5p-dialogcards-gone')) {
+      if (!this.params.behaviour.scaleTextNotCard || !isMobileLandscape || !displayLimits?.height || !this.$cardwrapperSet.find('.h5p-dialogcards-summary-screen').hasClass('h5p-dialogcards-gone')) {
         // Reset card-wrapper-set height
         this.$cardwrapperSet.css('height', 'auto');
 
@@ -815,7 +840,9 @@ class Dialogcards extends H5P.EventDispatcher {
         this.$cardwrapperSet.css('height', relativeMaxHeight + 'em');
       }
       else {
-        this.$cardwrapperSet.css('height', `${displayLimits?.height / 2}px`);
+        if (displayLimits?.height - nonCardHeight > 1) {
+          this.$cardwrapperSet.css('height', `${displayLimits?.height - nonCardHeight}px`);
+        }
       }
 
       this.scaleToFitHeight();
@@ -1019,7 +1046,7 @@ class Dialogcards extends H5P.EventDispatcher {
     let topWindow = this.getTopWindow();
 
     // iOS doesn't change screen dimensions on rotation
-    let screenSize = (this.isIOS() && window.orientation === 90) ?
+    let screenSize = (this.isIOS() && this.getOrientation() === 'landscape') ?
       { height: screen.width, width: screen.height } :
       { height: screen.height, width: screen.width };
 
@@ -1036,6 +1063,18 @@ class Dialogcards extends H5P.EventDispatcher {
   }
 
   /**
+   * Compute display limits for KidsLoop Live.
+   * @return {object|null} Height and width in px or null if cannot be determined.
+   */
+  computeDisplayLimitsKLL() {
+    const displayLimits = this.computeDisplayLimits();
+
+    // This only works because KLL enforces height on H5P's iframe
+    displayLimits.height = Math.min(displayLimits.height, document.body.offsetHeight);
+    return displayLimits;
+  }
+
+  /**
    * Detect whether user is running iOS.
    * @return {boolean} True, if user is running iOS.
    */
@@ -1044,6 +1083,53 @@ class Dialogcards extends H5P.EventDispatcher {
       ['iPad Simulator', 'iPhone Simulator', 'iPod Simulator', 'iPad', 'iPhone', 'iPod'].includes(navigator.platform) ||
       (navigator.userAgent.includes('Mac') && 'ontouchend' in document)
     );
+  }
+
+  /**
+   * Determine whether mobile device in landscape orientation.
+   * @return {boolean} True, if mobile device in landscape orientation.
+   */
+  isMobileLandscape() {
+    return this.isMobileDevice() && this.getOrientation() === 'landscape';
+  }
+
+  /**
+   * Detect mobile devices (http://detectmobilebrowsers.com/)
+   * @returns {boolean} True if running on a mobile device.
+   */
+  isMobileDevice() {
+    let check = false;
+    (function (a) {
+      if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw-(n|u)|c55\/|capi|ccwa|cdm-|cell|chtm|cldc|cmd-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc-s|devi|dica|dmob|do(c|p)o|ds(12|-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(-|_)|g1 u|g560|gene|gf-5|g-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd-(m|p|t)|hei-|hi(pt|ta)|hp( i|ip)|hs-c|ht(c(-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i-(20|go|ma)|i230|iac( |-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|-[a-w])|libw|lynx|m1-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|-([1-8]|c))|phil|pire|pl(ay|uc)|pn-2|po(ck|rt|se)|prox|psio|pt-g|qa-a|qc(07|12|21|32|60|-[2-7]|i-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h-|oo|p-)|sdk\/|se(c(-|0|1)|47|mc|nd|ri)|sgh-|shar|sie(-|m)|sk-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h-|v-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl-|tdg-|tel(i|m)|tim-|t-mo|to(pl|sh)|ts(70|m-|m3|m5)|tx-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas-|your|zeto|zte-/i.test(a.substr(0, 4))) check = true;
+    })(navigator.userAgent || navigator.vendor || window.opera);
+    return check;
+  }
+
+  /**
+   * Get device orientation.
+   * @return {string} 'portrait' or 'landscape'.
+   */
+  getOrientation() {
+    if (screen.orientation && screen.orientation.type) {
+      if (screen.orientation.type.includes('portrait')) {
+        return 'portrait';
+      }
+      else if (screen.orientation.type.includes('landscape')) {
+        return 'landscape';
+      }
+    }
+
+    // Unreliable, as not clear what device's natural orientation is
+    if (typeof window.orientation === 'number') {
+      if (window.orientation === 0 || window.orientation === 180) {
+        return 'portrait';
+      }
+      else if (window.orientation === 90 || window.orientation === -90 || window.orientation === 270) {
+        return 'landscape';
+      }
+    }
+
+    return 'landscape'; // Assume default
   }
 
   /**
@@ -1082,14 +1168,15 @@ class Dialogcards extends H5P.EventDispatcher {
       return;
     }
 
+    const isMobileLandscape = this.isMobileLandscape();
     this.cards.forEach(card => {
       if (this.params.mode === 'repetition') {
-        card.$buttonTurn.text(window.orientation === 90 ? '' : this.params.answer);
-        card.$buttonCorrect.text(window.orientation === 90 ? '' : this.params.correctAnswer);
-        card.$buttonIncorrect.text(window.orientation === 90 ? '' : this.params.incorrectAnswer);
+        card.$buttonTurn.text(isMobileLandscape ? '' : this.params.answer);
+        card.$buttonCorrect.text(isMobileLandscape ? '' : this.params.correctAnswer);
+        card.$buttonIncorrect.text(isMobileLandscape ? '' : this.params.incorrectAnswer);
       }
 
-      if (window.orientation !== 90) {
+      if (!isMobileLandscape) {
         this.$cardwrapperSet.css('max-height', '');
       }
     });
